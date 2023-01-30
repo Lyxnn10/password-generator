8d9b56d255d91d6ab5333b8858056f8e9246ce31
Lyxnn10 <93597031+Lyxnn10@users.noreply.github.com>
2023-01-30 21:28:04 +0100

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,6 +2,7 @@ import random
 
 chars = "abcdefghijklmnopqfstuvwyxz"
 numbers = "123456789"
+symbols = "#-_.!"
 
 while 1:
     password_len = int(input("What length would you like your password to be : "))
@@ -9,6 +10,6 @@ while 1:
     for x in range(0,password_count):
         password = ""
         for x in range(0,password_len):
-            password_total = random.choice(chars + numbers)
+            password_total = random.choice(chars + numbers +symbols)
             password      = password + password_total
         print("Here is your password : " + password)
